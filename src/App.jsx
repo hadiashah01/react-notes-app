0f9bb708002e85889ba1011fd088241c965f6096
Hadia <hadiashah010@gmail.com>
2026-09-13 19:46:44 +0500

    feat: create Navbar component and integrate it into App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="h-screen bg-[#0f131c] text-[#dfe2ee]">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+     <div className="navbar">
+          <h2 className="">NoteCraft</h2>
+          <h2 className="">U</h2>
+    </div>
+  )
+}
+
+export default Navbar
