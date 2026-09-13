62ba6b35a29c188703371addf403349751acc350
Hadia <hadiashah010@gmail.com>
2026-09-13 23:50:15 +0500

    feat: add AddNotesCard component and integrate it into App; update Navbar styling for responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import AddNotesCard from "./components/AddNotesCard";
 
 const App = () => {
   return (
-    <div className="h-screen bg-[#0f131c] text-[#dfe2ee] pt-0.5">
+    <div className="h-screen bg-[#0f131c] text-[#dfe2ee] pt-0.5 ">
       <Navbar />
+      <AddNotesCard />
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NotepadText } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <div className="navbar flex justify-between px-25 p-4  border-b-[0.1px] border-b-[#4e4e4e] ">
+    <div className="navbar flex justify-between lg:px-25 p-4  border-b-[0.1px] border-b-[#4e4e4e] ">
       <div className="brand flex items-center gap-2">
         <NotepadText  className="p-1 border border-[#0b3af8] rounded-lg bg-blue-950 " size={29} color="#0b3af8" strokeWidth={2.5} />
         <h2 className="text-[1.2rem] font-bold">NoteCraft</h2>
