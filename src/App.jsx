d01c3ea254c60cd9a9748bdac6e7937ad8915f44
Hadia <hadiashah010@gmail.com>
2026-09-14 07:00:34 +0500

    feat: enhance task display with conditional rendering and improve button styling in App component

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ const App = () => {
               }}
             ></textarea>
             <div className="btn">
-              <button className="bg-[#c0c1ff] ml-auto flex items-center gap-2 py-2 px-5 font-semibold rounded-sm text-[#1000a9] mb-5 mr-5">
+              <button className="bg-[#c0c1ff] ml-auto active:scale-x-95 flex items-center gap-2 py-2 px-5 font-semibold rounded-sm text-[#1000a9] mb-5 mr-5">
                 <Plus size={14} color="#1000a9" strokeWidth={3} />
                 Add Note
               </button>
@@ -67,45 +67,50 @@ const App = () => {
             <h1 className="text-[21px] font-bold ">Recent Notes</h1>
           </div>
           <div className=" flex items-start gap-4 lg:gap-0 flex-wrap min-h-[29vh]">
-            {task.map((elem, idx) => {
-              return (
-                <section
-                  key={idx}
-                  className="lg:min-w-full sm:min-w-full pt-6 pb-5 mb-5 px-6 mt-2 flex flex-col border border-[#5555544d]  rounded-lg bg-[#181c24]  md:min-w-[47%] md:max-w-[47%]"
-                >
-                  <div className="flex gap-2 items-center">
-                    <h4 className="text-lg font-semibold flex-wrap text-wrap ">
-                      {elem.title}
-                    </h4>
-                    <div className="text-[13px] tracking-widest ml-auto">
-                      Just now
+            {task.length > 0 ? (
+              task.map((elem, idx) => {
+                return (
+                  <section
+                    key={idx}
+                    className="lg:min-w-full sm:min-w-full pt-6 pb-5 mb-5 px-6 mt-2 flex flex-col border border-[#5555544d]  rounded-lg bg-[#181c24]  md:min-w-[47%] md:max-w-[47%]"
+                  >
+                    <div className="flex gap-2 items-center">
+                      <h4 className="text-lg font-semibold flex-wrap text-wrap ">
+                        {elem.title}
+                      </h4>
+                      <div className="text-[13px] text-nowrap tracking-widest ml-auto">
+                        Just now
+                      </div>
                     </div>
-                  </div>
-                  <p className="leading-6 tracking-tight mb-5 text-[#c7c4d7] mt-3 flex-wrap">
-                    {elem.notes}
-                  </p>
+                    <p className="leading-6 tracking-tight mb-5 text-[#c7c4d7] mt-3 flex-wrap">
+                      {elem.notes}
+                    </p>
 
-                  <div className=" border-t  border-t-[#5353534d]">
-                    <button className=" ml-auto mt-2 flex items-center gap-4 rounded-sm mr-3">
-                      <Pencil
-                        className=""
-                        size={18}
-                        strokeWidth={2}
-                        color="#908fa0"
-                      />
-                      <Trash
-                        onClick={(key) => {
-                          deleteTask(key);
-                        }}
-                        size={18}
-                        strokeWidth={2}
-                        color="#908fa0"
-                      />
-                    </button>
-                  </div>
-                </section>
-              );
-            })}
+                    <div className=" border-t  border-t-[#5353534d]">
+                      <button className=" ml-auto mt-2 flex items-center gap-4 rounded-sm mr-3">
+                        <Pencil
+                          className=""
+                          size={18}
+                          strokeWidth={2}
+                          color="#908fa0"
+                        />
+                        <Trash
+                          onClick={(key) => {
+                            deleteTask(key);
+                          }}
+                          className="active:scale-y-125 "
+                          size={18}
+                          strokeWidth={2}
+                          color="#908fa0"
+                        />
+                      </button>
+                    </div>
+                  </section>
+                );
+              })
+            ) : (
+              <div className="text-gray-500 ml-6 ">No tasks available</div>
+            )}
           </div>
         </div>
       </div>
